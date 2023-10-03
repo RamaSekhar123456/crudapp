22f8d9165bfb28e98997daed6f88ccd1f1ae6992
RamaSekhar123456 <ramasekhar.dhadhi@gmail.com>
2023-10-03 20:48:38 +0530

    first commit

---
+++ src/appData.js
@@ -0,0 +1,66 @@
+export const rowData = [
+    {
+        id : 1, 
+        title : 'Car',
+        price : 10,
+        company : 'Toyota',
+        info : "one of the best car",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 2, 
+        title : 'Bike',
+        price : 20,
+        company : 'Honda',
+        info : "Good bike",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 3, 
+        title : 'Book',
+        price : 50,
+        company : 'Book Angecies',
+        info : "Nice book  to read",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 4, 
+        title : 'Cycles',
+        price : 60,
+        company : 'Frog',
+        info : "Best for health",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 5, 
+        title : 'Tv',
+        price : 50,
+        company : 'LG',
+        info : "Nice Cleartiy",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 6, 
+        title : 'Computer',
+        price : 60,
+        company : 'Honda',
+        info : "Help to do programs",
+        inCart : false,
+        count : 1
+    },
+    {
+        id : 7, 
+        title : 'Laptop',
+        img : 'img/car7.jpg',
+        price : 60,
+        company : 'Lenovo',
+        info : "Good to do  multi tasking",
+        inCart : false,
+        count : 1
+    },
+]
